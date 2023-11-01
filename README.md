eb702dad767ab26b6265b54b010d39969e0c9eb0
Kris French <frenchie43972@gmail.com>
2023-11-01 18:15:54 +0900

    making progress

--- README.md
+++ README.md
@@ -29,3 +29,21 @@ vanilla-yahtzee
 	postcss.config.js
 	README.md
 	tailwind.config.js
+
+	*Top Section Scores*
+	I use twos as an example for this. It applies for to all the numbers
+	'twos': represents the property key for the function that calculates
+	 the score for all twos rolled.
+
+	 dice => is an arrow function that takes dice as its parameter. The dice parameter is 
+	 expected to be an array of integers representing the face-up value of each die
+
+	 dice.filter(val => val === 2) -  uses the filter method on the array. It creates a new
+	 array containing only the elements that equal 2. So if your dice array was [2, 3, 4, 2, 5], 
+	 this new array would be [2, 2].
+
+	 .reduce((a, b) => a + b, 0) - The reduce method takes a function and an initial value as arguments 
+	 so the  function ((a, b) => a + b) is applied to each element of the array. a is the accumulator 
+	 (starts at 0), and b is the current value in the array. This function adds up all the elements in 
+	 the array. So, for [2, 2], it would be 2 + 2 = 4
+	 *End To Section Scores*
--- scripts/modules/scorecard.js
+++ scripts/modules/scorecard.js
@@ -1,4 +1,40 @@
 export function scorecard() {
-    // alert("Hi there!");
+    
 }
 
+const scoreCalculators = {
+    // Check *Top Section Scores* in the README for greater details
+    // the dice arrow function represents an array of integers representing the number value
+    // and filters the specific numbers the uses the reduce method to create a new array and adds
+    // the new array up for total dice total
+    'ones': dice => dice.filter(val => val === 1).reduce((a, b) => a + b, 0),
+    'twos': dice => dice.filter(val => val === 2).reduce((a, b) => a + b, 0),
+    'threes': dice => dice.filter(val => val === 3).reduce((a, b) => a + b, 0),
+    'fours': dice => dice.filter(val => val === 4).reduce((a, b) => a + b, 0),
+    'fives': dice => dice.filter(val => val === 5).reduce((a, b) => a + b, 0),
+    'sixes': dice => dice.filter(val => val === 6).reduce((a, b) => a + b, 0),
+        //   **END TOP SECTION**
+
+    // Check *Full House* in the README for greater details
+    // 
+    'fullHouse': dice => {
+        const counts = {};
+        dice.forEach(val => counts[val] = (counts[val] || 0) + 1);
+        const keys = Object.keys(counts);
+        if (keys.length === 2 && (counts[keys[0]] === 2 || counts[keys[0]] === 3)) {
+            return 25;
+        }
+        return 0;
+    }, 
+};
+
+function calculateAndUpdateScore(scoreId) {
+    let diceValues = Array.from(document.querySelectorAll('.dice')).map(dice => parseInt(dice.getAttribute('data-value')));
+    let score = 0;
+    
+    if (scoreCalculators.hasOwnProperty(scoreId)) {
+        score = scoreCalculators[scoreId](diceValues);
+    }
+
+    document.querySelector(`#${scoreId} .score-value`).innerText = score;
+}
